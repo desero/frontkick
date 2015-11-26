5f5e334e2eb3d26c30955dff48dc4bcd1c5f2586
desero <desero@desero.ro>
2015-11-26 15:09:17 +0200

    Bug fixes

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var mkdirp = require('mkdirp');
 
 var FrontKickGenerator = yeoman.generators.Base.extend({
     init: function () {
